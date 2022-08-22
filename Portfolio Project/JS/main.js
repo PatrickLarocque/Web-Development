b826d89b8e94cd36883edc3b9eb68fd123eaf259
Patrick Larocque <patricklarocque1@gmail.com>
2022-08-21 20:09:55 -0400

    Skills, Work, Contact and Footer sections completed.

--- Portfolio Project/JS/main.js
+++ Portfolio Project/JS/main.js
@@ -1,20 +1,55 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-
+/*=============== Modify Header On Scroll ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header');
+    // When scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SERVICES MODAL ===============*/
 
 
-/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+/*=============== MixItUp Filter ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
+/*========== Style the active filter button in the projects section ==========*/ 
+const filterArray = document.querySelectorAll(".work__item");
 
-/* Link active work */ 
+function activeFilter() {
+    filterArray.forEach(f => f.classList.remove("active-work"));
+    this.classList.add("active-work");
+}
 
-
-/*=============== SWIPER TESTIMONIAL ===============*/
-
+filterArray.forEach(f => f.addEventListener('click', activeFilter));
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+function activeScroll() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', activeScroll);
 
 /*=============== LIGHT DARK THEME ===============*/ 
 
